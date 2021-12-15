a19eefb63b8993711a20a223dc6af24d65b99628
J. Morales <2971622+jonatanmv@users.noreply.github.com>
2021-12-15 20:28:31 +0100

    Add files via upload

---
+++ euromillones.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+import logging
+import secrets
+
+# Logging
+logging.basicConfig(format='%(message)s')
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
+# "apuesta" y "estrellas" lists
+apuesta = []
+estrellas = []
+
+while len(apuesta) < 5:
+    number = secrets.randbelow(50)
+    if number !=0 and number not in apuesta:
+        apuesta.append(number)
+
+while len(estrellas) < 2:
+    number = secrets.randbelow(12)
+    if number != 0 and number not in estrellas:
+        estrellas.append(number)
+
+log.info("Apuesta = %s" % apuesta)
+log.info("Estrellas = %s" % estrellas)
